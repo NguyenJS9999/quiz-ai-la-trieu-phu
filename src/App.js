d3a9c5c6b24e3a8fbc7a2e2d94e86066a95a33af
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-08-31 17:43:04 +0700

    React Js buoi 8 Quiz-game react sua lai, bo sung useRef, useEffect

--- src/App.js
+++ src/App.js
@@ -1,64 +1,175 @@
+import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { quiz, MONEY_BONUS } from './mockdata';
 
 function App() {
+
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  const [startPage, setStartPage] = useState( true );
+  const [questionPage, setQuestionPage] = useState('d-none');
+  const [resultPage, setResultPage] = useState('d-none');
+  const [nextBtnDisable, setNextBtnDisable] = useState(true);
+
+  const [hideNextBtn, setHideNextBtn] = useState("d-block");
+  const [hideFinishBtn, setHideFinishBtn] = useState("d-none");
+
+  const [choiceBtnDisable, setChoiceBtnDisable] = useState(false);
+  const [score, setScore] = useState(0);
+  const [stateMoneyBonus, setMoneyBonus] = useState(0);
+
+  const [message, setMessage] = useState("");
+  // Câu hỏi hiện tại lấy ra từ DATA ở index 0 - 1 mảng
+  const current_question = quiz[currentQuestionNumber];
+
+  function pressStart() {
+    setStartPage( false );
+    setQuestionPage('d-block, d-flex')
+  }
+
+  function checkAnswer(e) { console.log('Kiểm tra kết quả')
+    let current_answer = quiz[currentQuestionNumber].ans; console.log('current_answer', current_answer); console.log('currentQuestionNumber Index', currentQuestionNumber)
+    
+    let choice = e.target.textContent; console.log('choice', choice)
+
+    if (choice === current_answer) { console.log('Trả lời đúng')
+      // alert('Bạn đã trả lời ĐÚNG')
+      setScore(score + 10);
+
+      
+    } else { console.log('Trả lời sai')
+      // alert('Bạn đã trả lời SAI')
+      let moneyBonusNum = MONEY_BONUS[currentQuestionNumber]; console.log('moneyBonusNum', moneyBonusNum)
+      setMoneyBonus( moneyBonusNum )
+    }
+
+    if (currentQuestionNumber === quiz.length - 1) {
+      setHideNextBtn('d-none');
+      setHideFinishBtn('d-block, d-flex');
+    };
+    setChoiceBtnDisable(true);
+    setNextBtnDisable(false);
+  }
+  // Nút chuyển câu
+  function nextQuestion() { console.log('Câu kế tiếp')
+    if (currentQuestionNumber < quiz.length - 1) {
+      setCurrentQuestionNumber(currentQuestionNumber + 1);
+      setChoiceBtnDisable(false);
+    };
+    setNextBtnDisable(true);
+  }
+  // Màn thông báo kết quả
+  function showResultPage() {
+    setQuestionPage('d-none');
+    setResultPage('d-block, d-flex');
+    (score > 0) ? setMessage("Chúc mừng bạn đã đã hoàn thành cuộc chơi!") : setMessage("Hơi tiếc bạn chưa đúng câu nào!")
+  }
+  // Nút chơi lại - thay đổi giao diện và các giá trị về ban đầu
+  function replay() {
+    setStartPage( true );
+    setResultPage('d-none');
+    setCurrentQuestionNumber(0);
+    setQuestionPage('d-none');
+    setNextBtnDisable('true');
+    setHideFinishBtn('d-none');
+    setScore(0);
+    setMessage('');
+    setChoiceBtnDisable(false);
+
+  }
   return (
   <>
-     <section className=" quiz-container   container-fluid ">
-        {/* <div class=" quiz-title-name ">Ai là triệu phú</div> */}
-        <div className=" infor-btn-points-gift container ">     
-          <span>
-            <div className=" number-of-question "> Câu hỏi: <b>1</b>/15 </div>&nbsp;
-          </span>
-          <span>
-            <div className=" money-bonus "><i className="fas fa-dollar-sign" />&nbsp;200.000 vnđ</div>&nbsp;
-            <div className=" helps ">
-              {/* Bỏ đi ngẫu nhiên 2 đáp án sai */}
-              <span><b>50:50</b></span>
-              {/* Trả lời random 4 câu trả lời - ko chắc */}
-              <span> <i className="fas fa-phone-volume" /> </span>
-              {/* Hỏi ý  kiến của khán giả - nhiều người-random 4 câu trả lời -  2 đáp án ko chắc  */}
-              <span> <i className="fas fa-users" /> </span>
-              {/* Hỏi ý kiến nhà thông thái - đáp án đúng 100% */}
-              <span> <i className="fas fa-graduation-cap" /> </span>
-            </div>
-          </span>
-        </div>
-        {/*  */}
-        <div className=" process   container ">
-          <div className=" actual-progress ">0%</div>
-        </div>
-        {/* Phần khung quiz */}
-        <div className=" quiz-form    container ">
-          <div className=" questions ">
-            <span>Câu hỏi</span>
+    {/* Màn hình bắt đầu che đi ban đầu */}
+    <div className={` start-page  container-fluid p-0 ${ startPage ? 'd-block' : 'd-none' }`}>
+      <button type="button" className="App-start-btn  btn btn-lg d-block mx-auto py-3 px-5 fs-3" onClick={pressStart}>Start</button>
+    </div>  
+
+    {/* Màn hình để chơi */}
+    <section className= { ` quiz-container  ${questionPage} container-fluid ` } >
+
+      <div className=" infor-btn-points-gift container ">     
+
+        <span>
+          <div className=" number-of-question "> Câu hỏi: <b>{currentQuestionNumber + 1}</b>/10 </div>&nbsp;
+        </span>
+
+        <span>
+          <div className=" money-bonus ">Điểm&nbsp;{score} &nbsp; <i className="fas fa-dollar-sign" /> {stateMoneyBonus} vnđ</div>&nbsp;
+
+          <div className=" helps ">
+            {/* Bỏ đi ngẫu nhiên 2 đáp án sai */}
+            <span><b>50:50</b></span>
+            {/* Trả lời random 4 câu trả lời - ko chắc */}
+            <span> <i className="fas fa-phone-volume" /> </span>
+            {/* Hỏi ý  kiến của khán giả - nhiều người-random 4 câu trả lời -  2 đáp án ko chắc  */}
+            <span> <i className="fas fa-users" /> </span>
+            {/* Hỏi ý kiến nhà thông thái - đáp án đúng 100% */}
+            <span> <i className="fas fa-graduation-cap" /> </span>
           </div>
-          <div className=" answers  choices-box" id="content">
-            <div className=" answer " id="option-1">
-              <span className="numerical-order ">1</span>
-              <div>Đáp án A: </div>
-            </div>
-            <div className=" answer " id="option-2">
-              <span className="numerical-order ">2</span>
-              <div>Đáp án B:</div>
-            </div>
-            <div className=" answer " id="option-3">
-              <span className="numerical-order ">3</span>
-              <div>Đáp án C:</div>
-            </div>
-            <div className=" answer " id="option-4">
-              <span className="numerical-order ">4</span>
-              <div>Đáp án D:</div>
-            </div>
-          </div>
-          <div className=" buttons ">
-            <span className=" skip-question ">Câu tiếp</span>
-            <span className=" stop-quiz ">Dừng cuộc chơi?</span>
-          </div>
+
+        </span>
+
+      </div>
+
+      {/*  */}
+      <div className=" process   container ">
+        <div className=" actual-progress ">0%</div>
+      </div>
+
+      {/* Phần khung quiz */}
+      <div className=" quiz-form    container ">
+        {/* Câu hỏi */}
+        <div className=" questions ">
+          <span> {current_question.question} </span>
         </div>
-      </section>
-  
-  
-  
+        
+        <div className=" answers  choices-box" id="content">
+
+          <div onClick={checkAnswer} disabled={choiceBtnDisable} className=" answer " id="option-1">
+            <span className="numerical-order ">1</span>
+            <div> { current_question.choices[0] } </div>
+          </div>
+
+          <div onClick={checkAnswer} disabled={choiceBtnDisable} className=" answer " id="option-2">
+            <span className="numerical-order ">2</span>
+            <div> { current_question.choices[1] } </div>
+          </div>
+
+          <div onClick={checkAnswer} disabled={choiceBtnDisable} className=" answer " id="option-3">
+            <span className="numerical-order ">3</span>
+            <div> { current_question.choices[2] } </div>
+          </div>
+
+          <div onClick={checkAnswer} disabled={choiceBtnDisable} className=" answer " id="option-4">
+            <span className="numerical-order ">4</span>
+            <div> { current_question.choices[3] } </div>
+          </div>
+
+        </div>
+        
+        <div className=" buttons ">
+          <span className= { ` ${ hideNextBtn } skip-question `} onClick={nextQuestion} disabled={nextBtnDisable}  >&nbsp;Câu tiếp&nbsp;</span>
+          <span className={`${hideFinishBtn} stop-quiz `} onClick={showResultPage} >Finish</span>
+
+        </div>
+      </div>
+
+    </section>
+    {/* Màn hình để chơi */}
+
+    <div className={` result-page   container-fluid p-0 ${resultPage}`}>
+      <span>
+        <p className='h2 text-center'>{message}</p>
+        <p className='h3 text-center'>
+          <span>Số tiền bạn nhận được: { stateMoneyBonus }</span> <br/>
+          Và <span className="text-success">{score}</span> điểm 
+          
+        </p>
+
+        <button type="button" className="App-start-btn " onClick={replay}>Replay</button>
+      </span>
+    </div>
+      
   
   </>
   );
---
+++ src/mockdata.js
@@ -0,0 +1,113 @@
+export const quiz = [
+  {
+    id: 1,
+    question:
+      "Câu 1: Từ nào còn thiếu trong câu thành ngữ: 'Mật ngọt chết ...?'",
+    choices: ["A: Ong", "B: Ruồi", "C: Muỗi", "D: Kiến"],
+    ans: "B: Ruồi",
+  },
+  {
+    id: 2,
+    question:
+      "Câu 2: Loại bột nào sau đây không được dùng để chế biến thức ăn?",
+    choices: [" A: Bột mì ", " B: Bột gạo ", " C: Bột giặt ", " D: Bột năng "],
+    ans: " C: Bột giặt ",
+  },
+  {
+    id: 3,
+    question: "Câu 3: Từ nào sau đây viết đúng chính tả tiếng Việt? ",
+    choices: [
+      " A: Trậm chạp ",
+      " B: Chậm Trạp ",
+      " C: Trậm trạp ",
+      " D: Chậm chạp ",
+    ],
+    ans: " D: Chậm chạp ",
+  },
+  {
+    id: 4,
+    question: "Câu 4: Loài vật nào thường được nuôi để lấy ngọc ",
+    choices: ["A: Cua ", " B: Mực ", " C: Trai ", " D: Sứa "],
+    ans: " C: Trai ",
+  },
+  {
+    id: 5,
+    question:
+      "Câu 5: Trên mặt một đồng hồ cơ có số chỉ giờ, đối diện với số 9 là số mấy? ",
+    choices: [" A: Số 3", " B: Số 6 ", " C: Số 1  ", " D: Số 12 "],
+    ans: " A: Số 3 ",
+  },
+  {
+    id: 6,
+    question:
+      "Câu 6: Hiện nay, vùng an toàn  được nhiều khu dân cư, ngõ xóm thiết lập nhằm chống lây nhiễm COVID-19 vào khu đó thường được gọi là gì? ",
+    choices: [
+      " A: Vùng nâu ",
+      " B: Vùng nâu ",
+      " C: Vùng xám ",
+      " D: Vùng xanh ",
+    ],
+    ans: " D: Vùng xanh ",
+  },
+  {
+    id: 7,
+    question:
+      "Câu 7: Bàn thắng đầu tiên tại  VCK UEFA Euro 2020 có điểm gì đặc biệt? ",
+    choices: [
+      " A: Ghi bàn từ giữa sân",
+      " B: Do thủ môn ghi bàn ",
+      " C: Là bàn phản lưới nhà ",
+      " D: Đá phạt góc thành bàn ",
+    ],
+    ans: " C: Là bàn phản lưới nhà ",
+  },
+  {
+    id: 8,
+    question:
+      "Câu 8: Ngôn ngữ nào trên thế giới còn được gọi là 'Ngôn ngữ Molière'? ",
+    choices: [
+      " A: Tiếng Pháp",
+      " B: Tiếng Anh ",
+      " C: Tiếng Đức ",
+      " D: Tiếng Ý ",
+    ],
+    ans: " A: Tiếng Pháp ",
+  },
+  {
+    id: 9,
+    question:
+      " Câu 9: Thành Tây Đô (nay ở Thanh Hóa)  được xây dựng vào triều đại nào trong lịch sử nước ta",
+    choices: [" A: Nhà Lý", " B: Nhà Trần", " C: Nhà Hồ", " D: Nhà Hậu Lê"],
+    ans: " B: Nhà Trần ",
+  },
+  {
+    id: 10,
+    question:
+      " Câu 10: Vào ngày 2/9/2020, hình ảnh quốc kỳ Việt Nam được chiếu lên toàn bộ bề mặt tòa nhà nổi tiếng thế giới nào?",
+    choices: [
+      " A: Tháp đôi Petronas",
+      " B: Tháp Đài Bắc 101 (Taipei 101)",
+      " C: Tháp Burj Khalifa",
+      " D: Tháp Eiffel",
+    ],
+    ans: " C: Tháp Burj Khalifa ",
+  },
+]; // QUESTIONS_DATA
+
+export const MONEY_BONUS = [
+  "200,000",
+  "400,000",
+  "600,000",
+  "1,000,000",
+  "2,000,000",
+  "3,000,000",
+  "6,000,000",
+  "10,000,000",
+  "14,000,000",
+  "22,000,000",
+  "30,000,000",
+  "40,000,000",
+  "60,000,000",
+  "85,000,000",
+  "150,000,000",
+];
